c87efbf99a5bb0d08447178597921e7b930150c3
U039b <esther@pts-project.org>
2026-03-09 15:39:06 +0100

    Update GeoIP details component

--- src/components/geoip/GeoIPDetails.vue
+++ src/components/geoip/GeoIPDetails.vue
@@ -18,15 +18,11 @@ export default defineComponent({
     </tr>
     <tr>
       <td class="text-left">Organisation</td>
-      <td class="text-end fst-italic text-primary">{{ geoip.asnOrganization }}</td>
+      <td class="text-end fst-italic text-primary">{{ geoip.organization }}</td>
     </tr>
     <tr>
       <td class="text-left">ASN</td>
       <td class="text-end font-monospace small">{{ geoip.asn }}</td>
-    </tr>
-    <tr>
-      <td class="text-left">Network</td>
-      <td class="text-end font-monospace small">{{ geoip.asnNetwork }}</td>
     </tr>
     </tbody>
   </table>
